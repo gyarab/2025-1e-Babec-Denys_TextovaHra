ed4a9ffb92833d0835f19b89a4fd6ab60b319ae2
SuperWolf121 <denysmail55@gmail.com>
2026-03-10 08:43:46 +0100

    Zmena textu

--- randombull/CelaHra/HraMain.java
+++ randombull/CelaHra/HraMain.java
@@ -4,28 +4,38 @@ import java.util.Random;
 
 public class HraMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        uvodJmeno();
+        Hrac hrac = uvodJmeno();
+
+
 
 
     }
 
-
-    private static void uvodJmeno() {
+    private static Hrac uvodJmeno() {
         System.out.println();
         System.out.println();
         System.out.println("Hej bracho, vitej ve hre!");
         System.out.println();
         System.out.println("Abych ti nemusel rikat bracho, jak se jmenujes?");
         System.out.println();
+
         Hrac hrac = new Hrac();
         Scanner sc = new Scanner(System.in);
         hrac.jmeno = sc.nextLine();
+
         System.out.println();
-        System.out.println("Hmm, tak jo " + hrac.jmeno + " pokusim se ti vysvetlit jak to tady funguje.");
-        System.out.println("Mas inventar a v nem veci, take mas moznosti. Vzdy ti napisu jestli chces udelat to nebo to. Jo, mimochodem mas 10 hp (zivotu)");
-        System.out.println("Tahle hra ma vice koncu, takze si zkus to dohrat vickrat. A ted pojdme zacit.");
+        System.out.println("Tak jo " + hrac.jmeno +" pokusim se ti to vysvetlit");
+        System.out.println("""
+        Ve zkratce mas moznosti ze kterych si vyberes.
+        Taky mas 10 hp (zivotu).
+        Hodne stesti
+        """);
+
+
         System.out.println();
+        return hrac;
     }
 }
 
